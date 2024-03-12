88dde53e8aa157c0fb31bfd9128649536b2c424a
Rachid KABBAB <r.kabbab@gmail.com>
2024-03-12 15:37:20 -0700

    Commented API for deleting an ATM as we do not have to let a supervisor to delete ATM from the system

--- api/v1/views/atms.py
+++ api/v1/views/atms.py
@@ -42,11 +42,11 @@ def get_atm(atm_id):
         abort(404)
     return jsonify(atm.to_dict())
 
-
+'''
 @app_views.route('/atms/<atm_id>', methods=['DELETE'], strict_slashes=False)
 def delete_atm(atm_id):
     """
-    Delete a specific atm
+    Deletes a specific atm
     """
     atm = storage.get(ATM, atm_id)
     if not atm:
@@ -54,7 +54,7 @@ def delete_atm(atm_id):
     storage.delete(atm)
     storage.save()
     return make_response(jsonify({}), 200)
-
+'''
 
 @app_views.route('/branches/<branch_id>/atms', methods=['POST'], strict_slashes=False)
 def create_atm(branch_id):
